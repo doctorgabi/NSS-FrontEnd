6cc2be99658503edfaac6bcf8b8b48e5707660c2
Gabrielle Epstein <doctorgabi@gmail.com>
2013-10-14 00:20:34 -0500

    address book project working version with mailto and remove glitches

--- 2013-10-13-Address-Book/js/app/app.js
+++ 2013-10-13-Address-Book/js/app/app.js
@@ -14,11 +14,12 @@ function initialize(){
   Δdb = new Firebase('https://address-book-gse.firebaseio.com/');
   Δpeople = Δdb.child('people');
   $('#add').click(addPerson);
+  Δpeople.on('child_added', updatePeopleView);
+  $('.name').dblclick(removePerson);
 }
 
 
 function addPerson(){
-  debugger;
   var name = $('#name').val();
   var address = $('#address').val();
   var website = $('#website').val();
@@ -27,58 +28,44 @@ function addPerson(){
 
   var person = {};
 
-  person.name = name
-  person.address = address
-  person.website = website
-  person.email = email
-  person.photo = photo
+  person.name = name;
+  person.address = address;
+  person.website = website;
+  person.email = email;
+  person.photo = photo;
 
   Δpeople.push(person);
+  $('#name').val('');
+  $('#name').focus();
+  $('#address').val('');
+  $('#website').val('');
+  $('#email').val('');
+  $('#photo').val('');
+
+}
+
+function updatePeopleView(snapshot){
+  var key = snapshot.name();
+  var person = snapshot.val();
+  db.people.push(person);
+  var box = '<div class="person"><h5 class="name"></h5><img class="img"/><p class="address"></p><a class="website">visit website</a><p></p><a class="email"></a><p class="remove">(double-click name to remove person)</p></div>';
+  var $box = $(box);
+
+  $box.children('.name').text(person.name);
+  $box.children('.img').attr('src', person.photo);
+  $box.children('.address').text(person.address);
+  $box.children('.website').attr('href', person.website);
+  $box.children('.email').attr('mailto', person.email).text(person.email);
+
+  $('#people').prepend($box);
+}
+
+function removePerson(){
+  alert('the click handler works!')
+  debugger;
+  $(this).parent().remove();
 }
 
 
 
 
-
-
-
-
-
-//   Δperson.on('value', personChanged);
-
-
-// function itemAdded(snapshot){
-//   var item = snapshot.val();
-//   createRow(item);
-//   updateGrandTotal(item);
-//   db.items.push(item);
-// }
-
-// function personChanged(snapshot){
-//   var person = snapshot.val();
-
-//   try{
-//     $('#person').val(person.fullName);
-//     $('#address').val(person.address);
-//     db.person = person;
-//   } catch(e) {
-//     console.log(e);
-//   }
-// }
-
-
-
-
-// function createRow(item){
-//   var row = '<tr><td class="name"></td><td class="count"></td><td class="value"></td><td class="room"></td><td class="condition"></td><td class="date"></td></tr>';
-//   var $row = $(row);
-
-//   $row.children('.name').text(item.name);
-//   $row.children('.count').text(item.count);
-//   $row.children('.value').text('$' + item.value + '.00');
-//   $row.children('.room').text(item.room);
-//   $row.children('.condition').text(item.condition);
-//   $row.children('.date').text(item.date);
-
-//   $('#items').append($row);
-// }
